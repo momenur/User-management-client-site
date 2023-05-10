1dc649e6ff83eb565302d4d1eaab49016573f768
momenur <momenurislam6@gmail.com>
2023-05-11 00:41:49 +0600

    added form

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,36 @@ function App() {
 
   const [users, setUsers] = useState([])
 
-  useEffect( () => {
+  useEffect(() => {
     fetch('http://localhost:5000/users')
-    .then(res => res.json())
-    .then(data => setUsers(data))
-  },[])
+      .then(res => res.json())
+      .then(data => setUsers(data))
+  }, [])
+
+  const handleAddUser = event => {
+    event.preventDefault();
+    const form = event.target;
+    const name = form.name.value;
+    const email = form.email.value;
+    console.log(name, email);
+  }
   return (
     <>
       <h1>Users Management Client Site {users.length}</h1>
+
+      <form onSubmit={handleAddUser}>
+        <input type="text" name='name' />
+        <br />
+        <input type="email" name='email' />
+        <br />
+        <input type="submit" value="Add User" />
+      </form>
+
+      <div>
+        {
+          users.map(user => <p key={user.id}>id: {user.id} Name:{user.name} Gmail: {user.email}</p>)
+        }
+      </div>
     </>
   )
 }
